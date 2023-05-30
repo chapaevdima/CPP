82f8a31ae0258781b7133d228cec2709c98f79eb
Andy Natasha <anatasha@c2r9s3.42yerevan.am>
2023-05-30 19:01:54 +0400

    upd cpp04

--- cpp04/ex02/Animal.cpp
+++ cpp04/ex02/Animal.cpp
@@ -30,11 +30,6 @@ Animal::~Animal()
 	std::cout << "Animal destructor called" << std::endl;
 }
 
-void Animal::makeSound() const
-{
-	std::cout << "Some animal sound" << std::endl;
-}
-
 std::string Animal::getType() const
 {
 	return this->type;
