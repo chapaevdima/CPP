b11ae610048acb7aea56d087409ed521c5304631
Dmitrii Chapaev <dima.chapaev2003@yandex.ru>
2023-08-17 21:18:28 +0400

    upd cpp09 ex02

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -7,53 +7,55 @@ void vectorSort(std::vector<int>& inv)
     if (inv.size() > 10)
     {
         std::vector<int>::iterator midIter = inv.begin() + ((inv.size())/2);
-        //std::cout << *midIter << std::endl;
 
         std::vector<int> v1(inv.begin(), midIter);
         std::vector<int> v2(midIter, inv.end());
 
-        
-
         vectorSort(v1);
         vectorSort(v2);
-        
-        for (unsigned int i = 0; i < v1.size(); i++)
-            std::cout << v1[i] << " ";
-        std::cout << std::endl;
-
-        for (unsigned int i = 0; i < v2.size(); i++)
-            std::cout << v2[i] << " ";
-        std::cout << std::endl;
-
-        
-        /*std::vector<int>::iterator it1 = v1.begin();
+                
+        std::vector<int>::iterator it1 = v1.begin();
         std::vector<int>::iterator it2 = v2.begin();
 
         unsigned int i = 0;
-        while(it1 != v1.end() || it2 != v2.end())
+        while((it1 != v1.end() && it2 != v2.end()))
         {
-            if(it1 != v1.end() && *it1 < *it2)
+            if(*it1 < *it2)
                 inv[i] = *(it1++);
             else
                 inv[i] = *(it2++);
             i++;
-        }*/
-    }
+        }
 
-    unsigned int i = 0;
-    while(i < inv.size() - 1)
+		while (it1 != v1.end())
+			inv[i++] = *(it1++);
+
+		while (it2 != v2.end())
+			inv[i++] = *(it2++);		
+		
+    } else
     {
-        if(inv[i] > inv[i+1])
+        unsigned int i = 0, j = 0;
+		bool flag = true;
+        while(i < inv.size() - 1)
         {
-            int tmp = inv[i];
-            inv[i] = inv[i + 1];
-            inv[i + 1] = tmp;
-            if(i != 0)
-                i--;
-        } else
-            i++;
+            if(inv[i] > inv[i+1])
+            {
+				if(flag)
+					j = i;
+				flag = false;
+                int tmp = inv[i];
+                inv[i] = inv[i + 1];
+	            inv[i + 1] = tmp;
+	            if(i != 0)
+	                i--;
+	        } else
+	        {
+				i = j;
+				flag = true;
+			} 
+	    }
     }
-
 }
 
 void listSort(std::list<int>& inl)
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -17,19 +17,19 @@ int main (int argc, char **argv)
         int_list.push_back(std::atoi(argv[i]));
     }
 
-    /*std::cout << "Before: ";
+    std::cout << "Before: ";
     for (unsigned int i = 0; i < int_vector.size(); i++)
         std::cout << int_vector[i] << " ";
     std::cout << std::endl;
 
-    std::cout << "Before: ";
+    /*std::cout << "Before: ";
     std::list<int>::iterator it = int_list.begin();
     for (; it != int_list.end(); ++it)
         std::cout << *it << " ";
     std::cout << std::endl;*/
 
 	vectorSort(int_vector);
-	std::cout << "After: ";
+	std::cout << "After : ";
     for (unsigned int i = 0; i < int_vector.size(); i++)
         std::cout << int_vector[i] << " ";
     std::cout << std::endl;
